45e4f95db7c3f5de6badc220128f660b9f0c0da3
Vinay Kumar Gupta <sumar05082000@gmail.com>
2026-02-26 23:50:19 +0530

    added selenium browser setup

---
+++ Selenium/browserSetup.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://flipkart.com")
+driver.maximize_window()
+
+
+
+
+driver.quit()
